52a8029a8529c30b0e0137a68b76d0a63cd8be81
GuangyaoSHI <33379281+GuangyaoSHI@users.noreply.github.com>
2021-11-07 22:56:36 -0500

    add a matlab file on quaternion based complimentary filter

---
+++ enae788m_f21/assignment2/quaternion_complementaryt_filter.m
@@ -0,0 +1,7 @@
+syms q0 qx qy qz
+
+R_q = [2*q0**2+2*qx**2-1,     2*qx*qy-2*q0*qz,         2*qx*qz+2*q0*qy;
+       2*qx*qy+2*q0*qz,       2*q0**2+2*qy**2-1,       2*qy*qz-2*q0*qx;
+       2*qx*qz-2*q0*qy,       2*qy*qz+2*q0*qx,         2*q0**2+2*qz**2-1
+       ]
+   
